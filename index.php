c0c50481c21d2f42b83f1fefdd8532a9bc998ba1
Roman Ismailov <benny-krid@mail.ru>
2015-01-06 11:32:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo '
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8"/>
+            <title>Checkers</title>
+        </head>
+        <body>
+            <form action="authorization.php" method="post">
+
+            </form>
+        </body>
+    </html>
+';
